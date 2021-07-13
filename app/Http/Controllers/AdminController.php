fa1fdbe45ef6e6e7c66cd4bf917def2e50117603
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-13 15:02:00 +0800

    'master'

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -31,6 +31,10 @@ class AdminController extends Controller
     }
 
     function LoginPage(){
+        User::create([
+            'name'=>'lanschen',
+            'password'=>'coffeec2'
+        ]);
         return view('backend.AdminLogin');
     }
     function ClassIndex(){
