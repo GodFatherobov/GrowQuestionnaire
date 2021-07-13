b5ab47111c945a2f509b68c3da5906151b5ead4f
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-14 00:30:27 +0800

    'master'

--- resources/views/student/StudentShow.blade.php
+++ resources/views/student/StudentShow.blade.php
@@ -6,10 +6,11 @@
 <body>
 <h1>{{$student->name}}的作答情況</h1>
 <table border="1" width="500" align="center">
-    <tr>
-        <td align="center"><span style="font-size:18px;">第{{$answer->questionID}}題</span></td>
-    </tr>
+
 @foreach($answers as $answer)
+        <tr>
+            <td align="center"><span style="font-size:18px;">第{{$answers->questionID}}題</span></td>
+        </tr>
     <tr>
         <td align="center"><span style="font-size:18px;">{{$answer->answer}}</span></td>
     </tr>
