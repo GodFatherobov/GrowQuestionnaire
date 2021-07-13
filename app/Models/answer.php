6eedf3fcc158b464e116465370aeaabe235fd9ef
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-13 23:20:23 +0800

    'master'

--- app/Models/answer.php
+++ app/Models/answer.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class answer extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'studentID',
+        'questionID',
+        'answer',
+    ];
 }
