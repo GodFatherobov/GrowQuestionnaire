394e5801a481b08c710b19b28e7d1017d237947c
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-14 00:43:05 +0800

    'master'

--- resources/views/student/StudentShow.blade.php
+++ resources/views/student/StudentShow.blade.php
@@ -4,17 +4,17 @@
     <title>問卷後台</title>
 </head>
 <body>
+<div align="center">
 <h1>{{$student->name}}的作答情況</h1>
-<table border="1" width="500" align="center">
+<table border="1" width="300" align="center">
 
 @foreach($answers as $answer)
-        <tr>
-            <td align="center"><span style="font-size:18px;">第{{$answer->questionID}}題</span></td>
-        </tr>
     <tr>
+        <td align="center"><span style="font-size:18px;">第{{$answer->questionID}}題</span></td>
         <td align="center"><span style="font-size:18px;">{{$answer->answer}}</span></td>
     </tr>
 @endforeach
 </table>
+</div>
 </body>
 </html>
