a88940e01cb2b12e480230b743f295f86a0114e3
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-13 17:42:14 +0800

    'master'

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,7 @@ use App\Models\student;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 use Symfony\Component\Console\Question\Question;
 
