5daba1209c506f8cdd0398c1af4907f3ae27dd1d
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-15 02:51:35 +0800

    'master'

---
+++ database/migrations/2021_07_14_184743_create_weights_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWeightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weights', function (Blueprint $table) {
+            $table->id();
+            $table->integer('questionID');
+            $table->integer('S1');
+            $table->integer('S2');
+            $table->integer('S3');
+            $table->integer('S4');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weights');
+    }
+}
