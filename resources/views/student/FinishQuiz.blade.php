91f1b16b0e1b56211ffcdbd9dbdb173c5735a602
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-15 18:02:43 +0800

    'master'

--- resources/views/student/FinishQuiz.blade.php
+++ resources/views/student/FinishQuiz.blade.php
@@ -7,6 +7,6 @@
 <h1>感謝您的填寫 !</h1>
 <h2>1. LEAD領導力問卷分析報告會於課堂上分發。</h2>
 <h2 style="color:green">2.請您發送以下連結邀請他人給您回饋，其中可以包含上級主管、部屬、同事或其他，至少四位。</h2>
-<p><a href="http://growquestionnaire.herokuapp.com/others/{{$Sid}}">http://growquestionnaire.herokuapp.com/others/{{$Sid}}</a></p>
+<h2><a href="http://growquestionnaire.herokuapp.com/{{$Sid}}/others">http://growquestionnaire.herokuapp.com/{{$Sid}}/others</a></h2>
 </body>
 </html>
