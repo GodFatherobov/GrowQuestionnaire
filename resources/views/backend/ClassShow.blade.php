b90b784f4aebc9552b27ee01291dce90e4f6bb20
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-16 00:41:34 +0800

    'master'

--- resources/views/backend/ClassShow.blade.php
+++ resources/views/backend/ClassShow.blade.php
@@ -11,10 +11,12 @@
     <table border="1" width="200" align="center">
         <tr>
             <td align="center"><span style="font-size:18px;">學員姓名</span></td>
+            <td align="center"><span style="font-size:18px;">邀請問卷網址</span></td>
         </tr>
         @foreach($students as $student)
             <tr>
                 <td align="center"><span style="font-size:18px;"><a href=" {{ route('student.StudentShow', ['Sid' => $student->id]) }}">{{$student->name}}</a></span></td>
+                <td align="center"><span style="font-size:18px;"><a href="http://growquestionnaire.herokuapp.com/{{$Sid}}/others">http://growquestionnaire.herokuapp.com/{{$Sid}}/others</a></span></td>
             </tr>
         @endforeach
     </table>
