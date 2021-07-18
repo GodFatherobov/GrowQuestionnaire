7b38dc7e98bcb871318244a01cc07e5fea8a113b
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-19 00:59:35 +0800

    'master'

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -76,31 +76,31 @@ class StudentController extends Controller
         $sum=$S1+$S2+$S3+$S4;
 
         $img = Image::make(public_path('page2.png'));
-        $img->text($S1, 645, 1825, function($font) {
+        $img->text($S1, 645, 1820, function($font) {
             $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
             $font->size(36);
             $font->align('center');
             $font->valign('top');
         });
-        $img->text($S2, 755, 1825, function($font) {
+        $img->text($S2, 755, 1820, function($font) {
             $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
             $font->size(36);
             $font->align('center');
             $font->valign('top');
         });
-        $img->text($S3, 865, 1825, function($font) {
+        $img->text($S3, 865, 1820, function($font) {
             $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
             $font->size(36);
             $font->align('center');
             $font->valign('top');
         });
-        $img->text($S4, 975, 1825, function($font) {
+        $img->text($S4, 975, 1820, function($font) {
             $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
             $font->size(36);
             $font->align('center');
             $font->valign('top');
         });
-        $img->text($sum, 975, 1950, function($font) {
+        $img->text($sum, 975, 1945, function($font) {
             $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
             $font->size(36);
             $font->align('center');
