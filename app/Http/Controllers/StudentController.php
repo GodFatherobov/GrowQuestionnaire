f1fec4e2a8cd2410fd112bec737d0dfa61def296
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-20 00:14:09 +0800

    'master'

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -164,8 +164,8 @@ class StudentController extends Controller
         $S1=0;$S2=0;$S3=0;$S4=0;$count=0;
         foreach ($Oids as $Oid){
             $answers=answer::where('otherID',$Oid)->get();
-            $count = count($answer);
-            dd($answer);
+            $count = count($answers);
+            dd($answers);
             foreach ($answers as $answer){
                 if($answer->answer=='A'){
                     $S1=$S1+1;
