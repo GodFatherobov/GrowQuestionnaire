7523877caae4c41b891e31d2ac8f55f1844883d3
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-19 16:18:31 +0800

    'master'

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -56,6 +56,152 @@ class StudentController extends Controller
             'answers'=> $answers,
         ]);
     }
+    function OthersQuiz($Sid){
+        $student=student::find($Sid);
+        return view('student.OtherQuiz',[
+            'student'=> $student,
+        ]);
+    }
+    function InputType($Sid){
+        $other=other::create([
+            'studentID'=>$Sid,
+            'type'=>request('type'),
+        ]);
+        return \Redirect::route('student.OthersQuestion',['Sid' => $Sid,'Oid'=>$other->id,'Qid'=>13]);
+    }
+    function ShowOtherQuestion($Sid,$Oid,$Qid){
+        $question=question::find($Qid);
+        return view('student.OtherQuestion',[
+            'question'=>$question,
+            'Qid'=>$Qid,
+            'Sid'=>$Sid,
+            'Oid'=>$Oid,
+        ]);
+    }
+    function StoreOtherAnswer($Sid,$Oid,$Qid){
+        answer::updateOrCreate(
+            ['otherID'=>$Oid , 'questionID'=>$Qid],
+            ['answer'=>request('answer')]
+        );
+        $Qid++;
+        if($Qid<=24){
+            return \Redirect::route('student.OthersQuestion',['Sid' => $Sid,'Oid'=>$Oid,'Qid'=>$Qid]);
+        }
+        else
+            return('感謝您的填寫 !');
+    }
+    function OtherIndex($Sid){
+        $student=student::find($Sid);
+        $others=other::where('studentID',$Sid)->get();
+        return view('student.OtherIndex',[
+            'others'=> $others,
+            'student'=>$student,
+        ]);
+    }
+    function OtherShow($Oid){
+        $other=student::find($Oid);
+        $answers=answer::where('otherID',$Oid)->get();
+        return view('student.OtherShow',[
+            'other'=> $other,
+            'answers'=> $answers,
+        ]);
+    }
+    function Chart($Sid){
+        $pdf = PDF::loadView('student.page2_pdf',['Sid' => $Sid]);
+        $pdf->setPaper('A4');
+        return $pdf->stream();
+    }
+    function MakeChart1($Sid){
+        $answers=answer::where('studentID',$Sid)->get();
+        $S1=0;$S2=0;$S3=0;$S4=0;
+        foreach ($answers as $answer){
+            $weight=question::find($answer->questionID);
+            if($answer->answer=='A'){
+                $S1=$S1+$weight->S1;
+            }
+            if ($answer->answer=='B'){
+                $S2=$S2+$weight->S2;
+            }
+            if ($answer->answer=='C'){
+                $S3=$S3+$weight->S3;
+            }
+            if ($answer->answer=='D'){
+                $S4=$S4+$weight->S4;
+            }
+        }
+        $img = Image::make(public_path('page1.png'));
+        $img->text($S1, 204, 575, function($font) {
+            $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
+            $font->size(12);
+            $font->align('center');
+            $font->valign('top');
+        });
+        $img->text($S2, 240, 575, function($font) {
+            $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
+            $font->size(12);
+            $font->align('center');
+            $font->valign('top');
+        });
+        $img->text($S3, 276, 575, function($font) {
+            $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
+            $font->size(12);
+            $font->align('center');
+            $font->valign('top');
+        });
+        $img->text($S4, 312, 575, function($font) {
+            $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
+            $font->size(12);
+            $font->align('center');
+            $font->valign('top');
+        });
+        $Oids=other::where('studentID',$Sid)->pluck('id');
+        $S1=0;$S2=0;$S3=0;$S4=0;$count=0;
+        foreach ($Oids as $Oid){
+            $count=$count+1;
+            $answers=answer::where('otherID',$Oid)->get();
+            foreach ($answers as $answer){
+                $weight=question::find($answer->questionID);
+                if($answer->answer=='A'){
+                    $S1=$S1+$weight->S1;
+                }
+                if ($answer->answer=='B'){
+                    $S2=$S2+$weight->S2;
+                }
+                if ($answer->answer=='C'){
+                    $S3=$S3+$weight->S3;
+                }
+                if ($answer->answer=='D'){
+                    $S4=$S4+$weight->S4;
+                }
+            }
+        }
+        $S1=round($S1/$count);$S2=round($S2/$count);$S3=round($S3/$count);$S4=round($S4/$count);
+        $img->text($S1, 435, 567, function($font) {
+            $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
+            $font->size(12);
+            $font->align('center');
+            $font->valign('top');
+        });
+        $img->text($S2, 470, 567, function($font) {
+            $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
+            $font->size(12);
+            $font->align('center');
+            $font->valign('top');
+        });
+        $img->text($S3, 505, 567, function($font) {
+            $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
+            $font->size(12);
+            $font->align('center');
+            $font->valign('top');
+        });
+        $img->text($S4, 540, 567, function($font) {
+            $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
+            $font->size(12);
+            $font->align('center');
+            $font->valign('top');
+        });
+        return($img->response('png'));
+    }
     function MakeChart2($Sid){
         $answers=answer::where('studentID',$Sid)->get();
         $S1=0;$S2=0;$S3=0;$S4=0;
@@ -160,60 +306,5 @@ class StudentController extends Controller
             $font->valign('top');
         });
         return($img->response('png'));
-    }
-    function OthersQuiz($Sid){
-        $student=student::find($Sid);
-        return view('student.OtherQuiz',[
-            'student'=> $student,
-        ]);
-    }
-    function InputType($Sid){
-        $other=other::create([
-            'studentID'=>$Sid,
-            'type'=>request('type'),
-        ]);
-        return \Redirect::route('student.OthersQuestion',['Sid' => $Sid,'Oid'=>$other->id,'Qid'=>13]);
-    }
-    function ShowOtherQuestion($Sid,$Oid,$Qid){
-        $question=question::find($Qid);
-        return view('student.OtherQuestion',[
-            'question'=>$question,
-            'Qid'=>$Qid,
-            'Sid'=>$Sid,
-            'Oid'=>$Oid,
-        ]);
-    }
-    function StoreOtherAnswer($Sid,$Oid,$Qid){
-        answer::updateOrCreate(
-            ['otherID'=>$Oid , 'questionID'=>$Qid],
-            ['answer'=>request('answer')]
-        );
-        $Qid++;
-        if($Qid<=24){
-            return \Redirect::route('student.OthersQuestion',['Sid' => $Sid,'Oid'=>$Oid,'Qid'=>$Qid]);
-        }
-        else
-            return('感謝您的填寫 !');
-    }
-    function OtherIndex($Sid){
-        $student=student::find($Sid);
-        $others=other::where('studentID',$Sid)->get();
-        return view('student.OtherIndex',[
-            'others'=> $others,
-            'student'=>$student,
-        ]);
-    }
-    function OtherShow($Oid){
-        $other=student::find($Oid);
-        $answers=answer::where('otherID',$Oid)->get();
-        return view('student.OtherShow',[
-            'other'=> $other,
-            'answers'=> $answers,
-        ]);
-    }
-    function Chart($Sid){
-        $pdf = PDF::loadView('student.page2_pdf',['Sid' => $Sid]);
-        $pdf->setPaper('A4');
-        return $pdf->stream();
     }
 }
--- resources/views/student/page2_pdf.blade.php
+++ resources/views/student/page2_pdf.blade.php
@@ -4,6 +4,7 @@
     <title>後台管理</title>
 </head>
 <body>
+<img src="{{ route('student.MakeChart1',['Sid' => $Sid] )}}" alt="圖片替代文字">
 <img src="{{ route('student.MakeChart2',['Sid' => $Sid] )}}" alt="圖片替代文字">
 </body>
 </html>
