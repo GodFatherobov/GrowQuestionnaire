e27b4b16a22f7073259f84bcd0ff11069ce63154
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-19 19:26:48 +0800

    'master'

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -510,16 +510,16 @@ class StudentController extends Controller
             $font->align('center');
             $font->valign('top');
         });
-        $img->text($OverLead, 135, 425, function($font) {
+        $img->text($OverLead, 130, 430, function($font) {
             $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
-            $font->size(12);
+            $font->size(16);
             $font->align('center');
             $font->valign('top');
             $font->angle(90);
         });
-        $img->text($LessLead, 564, 353, function($font) {
+        $img->text($LessLead, 560, 355, function($font) {
             $font->file(public_path('OpenSans-SemiboldItalic.ttf'));
-            $font->size(12);
+            $font->size(16);
             $font->align('center');
             $font->valign('top');
             $font->angle(90);
