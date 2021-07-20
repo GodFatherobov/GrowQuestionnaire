d19b529962c76f2eade2b78070f508bd67475eda
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-20 20:44:49 +0800

    'master'

--- resources/views/student/output_pdf.blade.php
+++ resources/views/student/output_pdf.blade.php
@@ -4,7 +4,7 @@
     <title>後台管理</title>
 </head>
 <body>
-<img src="{{ route('student.MakeChart1',['Sid' => $Sid] )}}" alt="加載錯誤" width="713" height="1030">
+<img src="{{ route('student.MakeChart1',['Sid' => $Sid] )}}" alt="加載錯誤" width="740" height="1040">
 <img src="{{ route('student.MakeChart2',['Sid' => $Sid] )}}" alt="加載錯誤" width="713" height="1030">
 <img src="{{ route('student.MakeChart3',['Sid' => $Sid] )}}" alt="加載錯誤" width="713" height="1030">
 
