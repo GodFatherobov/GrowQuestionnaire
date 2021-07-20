8428d2eb916ac7a2b943d21259579359c57490da
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-20 14:56:19 +0800

    'master'

--- resources/views/student/output_pdf.blade.php
+++ resources/views/student/output_pdf.blade.php
@@ -4,8 +4,8 @@
     <title>後台管理</title>
 </head>
 <body>
-<img src="{{ route('student.MakeChart1',['Sid' => $Sid] )}}" alt="加載錯誤">
-<img src="{{ route('student.MakeChart2',['Sid' => $Sid] )}}" alt="加載錯誤">
-<img src="{{ route('student.MakeChart3',['Sid' => $Sid] )}}" alt="加載錯誤">
+<img src="{{ route('student.MakeChart1',['Sid' => $Sid] )}}" alt="加載錯誤" width="713" height="1030">
+<img src="{{ route('student.MakeChart2',['Sid' => $Sid] )}}" alt="加載錯誤" width="713" height="1030">
+<img src="{{ route('student.MakeChart3',['Sid' => $Sid] )}}" alt="加載錯誤" width="713" height="1030">
 </body>
 </html>
