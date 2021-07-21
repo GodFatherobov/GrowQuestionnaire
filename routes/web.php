3fe8ab7da22a2044b161cbe9c71bbf7f5c24cec1
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-21 20:54:11 +0800

    'master'

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {return view('welcome');});
 
 //Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/logo', [App\Http\Controllers\StudentController::class, 'logo'])->name('logo');
 
