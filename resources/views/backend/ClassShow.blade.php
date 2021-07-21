11a48070a561f3f7b19a1e77c91c0d55a55615b6
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-21 18:29:28 +0800

    'master'

--- resources/views/backend/ClassShow.blade.php
+++ resources/views/backend/ClassShow.blade.php
@@ -7,12 +7,14 @@
 <div align="center">
 <h1>課程名稱：{{$class->ClassName}}</h1>
     <p>課程問卷連結：<a href=" {{ route('student.StudentQuiz', ['ClassLink' => $class->ClassLink]) }}">http://growquestionnaire.herokuapp.com/{{$class->ClassLink}}/Quiz</a></p>>
-    <div style="position:relative">
+    <div class="Row">
+        <div class="Column">C1</div>
+        <div class="Column">C2</div>
+        <div class="Column">C3</div>
+    </div>
         <div style="color: blue;float:left">學員自評問卷統計</div>
         <div style="color: #38c172">已完成份數</div>
         <div style="color: red">未完成份數</div>
-        </div>
-    </div>
     <table border="1" width="500" align="center">
         <tr>
             <td align="center"><span style="font-size:18px;">學員姓名</span></td>
