909a93bab5052a173edf5152bd01ec4a717f6b83
GodFatherobov <h4099ju7ki8@yahoo.com.tw>
2021-07-21 18:34:53 +0800

    'master'

--- resources/views/backend/ClassShow.blade.php
+++ resources/views/backend/ClassShow.blade.php
@@ -6,15 +6,12 @@
 <body>
 <div align="center">
 <h1>課程名稱：{{$class->ClassName}}</h1>
-    <p>課程問卷連結：<a href=" {{ route('student.StudentQuiz', ['ClassLink' => $class->ClassLink]) }}">http://growquestionnaire.herokuapp.com/{{$class->ClassLink}}/Quiz</a></p>>
-    <div class="parent">
-        <div class="column">Column 1</div>
-        <div class="column">Column 2<br>Column 2<br>Column 2<br>Column 2<br></div>
-        <div class="column">Column 3</div>
-    </div>
-        <div style="color: blue">學員自評問卷統計</div>
-        <div style="color: #38c172">已完成份數</div>
-        <div style="color: red">未完成份數</div>
+    <p>課程問卷連結：<a href=" {{ route('student.StudentQuiz', ['ClassLink' => $class->ClassLink]) }}">http://growquestionnaire.herokuapp.com/{{$class->ClassLink}}/Quiz</a></p>
+    <table border="1" width="500" align="center">
+        <td><div style="color: blue">學員自評問卷統計</div></td>
+        <td><div style="color: #38c172">已完成份數</div></td>
+        <td><div style="color: red">未完成份數</div></td>
+    </table>
     <table border="1" width="500" align="center">
         <tr>
             <td align="center"><span style="font-size:18px;">學員姓名</span></td>
